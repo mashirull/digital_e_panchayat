579ecd607f3f0530664dee639ffebafef65839f4
mashirull <you@example.com>
2025-12-24 22:25:45 +0530

    add some line of code in user register routs

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,20 +1,44 @@
- import { NextResponse } from "next/server";
-
-
+import { NextResponse } from "next/server";
+import user from "@/models/user";
+import bcrypt from "bcryptjs";
 
 export async function POST(req:Request ){
 
     const {name , mobile , password} =  await req.json();
 
-    if(!name || !mobile || !password) {
+    try {
+        if(!name || !mobile || !password) {
+            return NextResponse.json({
+                message : "All fields are required!",
+            }, {status : 400});
+        }
+
+        const userExist =  await user.findOne({mobile});
+        if(userExist){
+            return NextResponse.json({
+                message : "User Already Exists"
+            }, {status : 400})
+        }
+
+        const hashedPassword =  await bcrypt.hash(password , 10);
+
+        await user.create({
+            name,
+            mobile,
+            password : hashedPassword
+        });
+
         return NextResponse.json({
-            message : "All fields are required!",
-        }, {status : 400});
-    }
+            message : "User create successfully!",
+            user,
+        }, {status : 201})
 
 
-
-
-
-    return NextResponse.json("hello");
+        
+    } catch (error) {
+        return NextResponse.json({
+            message : "Something went wrong!",
+            error : error
+        }, {status : 401})
+    } 
  }
